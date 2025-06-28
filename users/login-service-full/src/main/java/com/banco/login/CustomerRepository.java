1ba260024620dd0db71230682d6104d81b76b806
KAREN123GARCIA <kg6399930@gmail.com>
2025-06-28 09:41:48 -0500

    🔐 feat(users/login): aplicar mejoras de seguridad, validación y estructura

--- users/login-service-full/src/main/java/com/banco/login/CustomerRepository.java
+++ users/login-service-full/src/main/java/com/banco/login/CustomerRepository.java
@@ -1,7 +1,9 @@
 package com.banco.login;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByMail(String mail);
     Optional<Customer> findByMailAndHashPassword(String mail, String hashPassword);
 }
